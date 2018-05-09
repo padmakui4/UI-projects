d2790a7e168e841f89538099cab1e9223d5ca90a
Padma K <39098374+padmakui4@users.noreply.github.com>
2018-05-09 08:33:06 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var existingTasks = [
+var existingTasks = [ 
 	{"name": "Test Task #1", "date": "12/01/2012", "assigned": "John Doe" },
 	{"name": "Test Task #2", "date": "12/02/2012", "assigned": "John Doe" },
 	{"name": "Test Task #3", "date": "12/03/2012", "assigned": "John Doe" },
